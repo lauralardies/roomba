31828da16c46ae4692620514ef67ce50b63d8568
crltsnch <91721777+crltsnch@users.noreply.github.com>
2023-02-16 10:25:11 +0000

    Hemos eliminado la clase habitacion. Utilizamos canvas para dibujarla

--- main.py
+++ main.py
@@ -1,21 +1,15 @@
 from tkinter import *
 
-
-class Habitacion():
-  '''
-  Clase que define las dimensiones de nuestra habitación.
-  '''
-  def __init__(self, largo, ancho):
-    self.largo = largo
-    self.ancho = ancho
-
-habitacion = Habitacion(630, 500)  #Nuestras medidas sin el borde
-
 # Configuración de la raíz
 root = Tk()
 root.title('Aplicación Roomba') # Título ventana.
 root.resizable(0, 0) # Con este comando desactivamos la redimensión de la ventana.
-root.config(bg = 'light blue') # Color fondo. 
+root.config(bg = 'black') # Color fondo. 
+
+# Añadimos la habitación con la función Canvas que nos da una superficie sobre la que podemos dibujar figuras. Es decir, sobre el canvas que hemos creado 'habitacion' vamos a dibujar el mueble.
+
+habitacion = Canvas(width = 500, height= 630, bg = 'light blue')
+habitacion.pack()
 
 root.mainloop() # Bucle de la aplicación
 
