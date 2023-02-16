632e225dfca41503dc632bc1e1d79bf4c13a9114
lauralardies <91721558+lauralardies@users.noreply.github.com>
2023-02-16 12:04:38 +0000

    intento botón

--- main.py
+++ main.py
@@ -1,34 +1,41 @@
 from tkinter import *
 
-# Configuración de la raíz
-root = Tk()
-root.title('Aplicación Roomba') # Título ventana.
-root.resizable(0, 0) # Con este comando desactivamos la redimensión de la ventana.
-root.config(bg = 'black') # Color fondo. 
+class Aplicacion():
+  def __init__(self):
 
-# Añadimos la habitación con la función Canvas que nos da una superficie sobre la que podemos dibujar figuras. Es decir, sobre el canvas que hemos creado 'habitacion' vamos a dibujar el mueble.
-habitacion = Canvas(width = 500, height= 630, bg = 'lightyellow1')
-habitacion.pack()
+    # Configuración de la raíz.
+    self.root = Tk()
+    self.root.title('Aplicación Roomba') # Título ventana.
+    self.root.resizable(0, 0) # Con este comando desactivamos la redimensión de la ventana.
+    self.root.config(bg = 'black') # Color fondo. 
+  
+    # Añadimos la habitación con la función Canvas que nos da una superficie sobre la que podemos dibujar figuras. Es decir, sobre el canvas que hemos creado 'habitacion' vamos a dibujar el mueble.
+    self.habitacion = Canvas(width = 500, height= 630, bg = 'lightyellow1')
+    self.habitacion.pack()
+    
+    # Con la siguiente función pintamos el mueble sobre nuestro canvas 'habitación'.
+    self.mueble = self.habitacion.create_rectangle(101, 150, 191, 410, fill = 'navajowhite3')
 
-# Con la siguiente función pintamos el mueble sobre nuestro canvas 'habitación'.
-mueble = habitacion.create_rectangle(101, 150, 191, 410, fill = 'navajowhite3')
+    # Calculamos el área que tiene que limpiar el roomba. Para ello, dividimos la habitación en 4 rectángulos y calculamos su área.
+    self.area_total = self.area(500, 150) + self.area(101, 480) + self.area(309, 480) + self.area(220, 90)
+    
+    # Calculamos el tiempo estimado que tarda el roomba en limpiar la habitación con obstáculos. 
+    self.a = 0.929 # Suponemos que el roomba limpia 92,9 m^2 cada 100 min aproximadamente. Por eso, limpia 0,929 m^2/min.
+    self.tiempo = self.area_total/self.a # Obtenemos el tiempo en minutos.
+    
+    # Añadimos un botón que nos abra una ventana con el tiempo que tarda el roomba en limpiar nuestra habitación.
+    self.boton = Button(self.root, text = 'Ver tiempo de recorrido', command = self.segunda_ventana(self.tiempo))
+    self.boton.pack(side=RIGHT))
+    
+    self.root.mainloop() # Bucle de la aplicación.
 
-root.mainloop() # Bucle de la aplicación.
+  def area(self, base, altura):
+    '''
+    Función que devuelve el área de un rectángulo. Los datos introducidos van en cm y el resultado obtenido lo pasamos a m^2.
+    '''
+    return (base * altura)/10000
 
-# Una vez creada la ventana, el siguiente paso es calcular el área que tiene que limpiar el roomba. Para ello, dividimos la habitación en 4 rectángulos y calculamos su área.
+  def segunda_ventana(self, tiempo):
+    print('Se estima que Roomba tarda ', tiempo, ' minutos en limpiar esta habitación.')
 
-def area(base, altura):
-  '''
-  Función que devuelve el área de un rectángulo. Los datos introducidos van en cm y el resultado obtenido lo pasamos a m^2.
-  '''
-  return (base * altura)/10000
-
-area_total = area(500, 150) + area(101, 480) + area(309, 480) + area(220, 90)
-
-# Por último, calculamos el tiempo que tarda el roomba en limpiar la habitación con el mueble.
-
-a = 0,929 # Suponemos que el roomba limpia 92,9 m^2 cada 100 min aproximadamente. Por eso, limpia 0,929 m^2/min.
-
-tiempo = area_total/a # Obtenemos el tiempo en minutos.
-
-#Añadimos una etiqueta con el tiempo tarda el roomba en limpiar nuestra habitación
+aplicacion = Aplicacion()
