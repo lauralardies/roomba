16ffe5b062f4fd170f7a96096139254b5dc9cc10
crltsnch <91721777+crltsnch@users.noreply.github.com>
2023-02-16 10:35:37 +0000

    Creamos mueble y cambiamos colores

--- main.py
+++ main.py
@@ -8,8 +8,11 @@ root.config(bg = 'black') # Color fondo.
 
 # Añadimos la habitación con la función Canvas que nos da una superficie sobre la que podemos dibujar figuras. Es decir, sobre el canvas que hemos creado 'habitacion' vamos a dibujar el mueble.
 
-habitacion = Canvas(width = 500, height= 630, bg = 'light blue')
+habitacion = Canvas(width = 500, height= 630, bg = 'lightyellow1')
 habitacion.pack()
+
+# Con la siguiente función pintamos el mueble sobre nuestro canvas 'habitación'.
+mueble = habitacion.create_rectangle(101, 150, 191, 410, fill = 'navajowhite3')
 
 root.mainloop() # Bucle de la aplicación
 
