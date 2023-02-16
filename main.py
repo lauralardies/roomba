42fffce1e7178ea480cc4dcc025f3cb09caf5dfb
lauralardies <91721558+lauralardies@users.noreply.github.com>
2023-02-16 10:01:00 +0000

    configuración ventana

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 from tkinter import *
 
+# Configuración de la raíz
 root = Tk()
-root.mainloop()
+root.title('Aplicación Roomba') # Título ventana.
+root.resizable(0, 0) # Con este comando desactivamos la redimensión de la ventana.
+root.config(bg = 'light blue') # Color fondo. 
+
+root.mainloop() # Bucle de la aplicación
